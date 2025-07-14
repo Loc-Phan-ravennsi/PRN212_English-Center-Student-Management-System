d8a7a474f354bf96c29fb0cb002586f20d67d3b9
Loc <phangialoc2005@gmail.com>
2025-07-14 08:47:02 +0700

    Add project files.

---
+++ Project/LoginWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project
+{
+    /// <summary>  
+    /// Interaction logic for LoginWindow.xaml  
+    /// </summary>  
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            // Handle login logic here  
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
